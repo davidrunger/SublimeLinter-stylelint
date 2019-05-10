7da2ce7ba474467781e9f2bc452be96280f6b22d
David Runger <davidjrunger@gmail.com>
2019-05-10 07:34:11 -0700

    Hack it to work with vue

--- linter.py
+++ linter.py
@@ -1,11 +1,12 @@
 import json
+import os
 import re
 import logging
+import subprocess
 from SublimeLinter.lint import NodeLinter
 from SublimeLinter.lint.linter import LintMatch
 
 logger = logging.getLogger('SublimeLinter.plugin.stylelint')
-
 
 class Stylelint(NodeLinter):
 
@@ -22,6 +23,30 @@ class Stylelint(NodeLinter):
     defaults = {
         'selector': 'source.css - meta.attribute-with-value, source.sass, source.scss, source.less, source.sss'  # noqa 501
     }
+
+    def _communicate(self, cmd, code=None):
+        filename = cmd[-1]
+        file = open(filename, "r")
+        code = file.read()
+        file.close()
+
+        lines = code.split('\n')
+        self.line_of_style_tag = [x for x in range(len(lines)) if '<style' in lines[x]][0]
+
+        proc = subprocess.Popen(
+            cmd,
+            env=self.get_environment(self.get_view_settings()),
+            cwd=self.get_working_dir(self.settings) or os.path.realpath('.'),
+            stdin=-1,
+            stdout=-1,
+            stderr=-1,
+            startupinfo=None,
+            creationflags=0
+        )
+
+        code = code.encode('utf8')
+        out = proc.communicate(code)
+        return out[0].decode("utf-8")
 
     def find_errors(self, output):
         """
@@ -61,7 +86,7 @@ class Stylelint(NodeLinter):
 
         if data and 'warnings' in data:
             for warning in data['warnings']:
-                line = warning['line'] - self.line_col_base[0]
+                line = warning['line'] - self.line_of_style_tag - self.line_col_base[0]
                 col = warning['column'] - self.line_col_base[1]
                 text = warning['text'].replace('(' + warning['rule'] + ')', '')
                 text = text.rstrip()
